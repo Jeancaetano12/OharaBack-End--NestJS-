80624fc2c95ceb80799a555f6110dab9a3a9942d
Jean Caetano <caetanojean4@gmail.com>
2025-12-26 19:04:08 -0300

    REFACTOR: Atualizando configuração do cors

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin:['http://localhost:3001', 'http://localhost:3000'],
+    origin:['http://localhost:3001', 'http://localhost:3000', 'https://ohara-back-end.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
