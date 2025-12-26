ea3bc8eae4135598d65133b57c10c2d3c07ea5c5
Jean Caetano <caetanojean4@gmail.com>
2025-12-26 17:34:54 -0300

    FIX: Consertando a configuração do cors

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin:['http://localhost:3001', 'https://ohara-front-end.vercel.app'],
+    origin:['http://localhost:3001', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
