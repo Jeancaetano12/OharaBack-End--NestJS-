3710aaff0cf88fdb7220d7e91cb58335bc325449
Jean Caetano <caetanojean4@gmail.com>
2026-02-09 03:02:35 -0300

    FIX: Corrigindo as dependencias e configurando o modulo da aplicação para exibir a documentação do swagger em produção.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,19 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [MembrosModule, CargosModule, AuthModule, UsersModule,
     ThrottlerModule.forRoot([{
       ttl: 60,
       limit: 12,
-    }])],
+    }]),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
+    })],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,
